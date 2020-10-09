9b98ede5a2be9da12261efd2ccddc2af2714990c
malikkorucu <malikkorucu@gmail.com>
2020-10-09 16:41:39 +0300

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const router = require('./router')
 const db = require('./helpers/db_connect')
-const customErrorHandler = require('./middlewares/ErrorHandler')
+const customErrorHandler = require('./middlewares/errorHandler')
 
 const PORT = process.env.PORT || 3500
 
@@ -11,16 +11,16 @@ app.use(cors())
 app.options("*" , cors())
 app.use(express.json())
 
-// db.getConnection((err, conn) => {
-//     // bağlantı kontrolü
-//     console.log('db bağlandı')
-//     conn.query();
-//     // bağlantıyı canlı tutan ifade !!!
-//     db.releaseConnection(conn);
-// })
+db.getConnection((err, conn) => {
+    // bağlantı kontrolü
+    console.log('db bağlandı')
+    conn.query();
+    // bağlantıyı canlı tutan ifade !!!
+    db.releaseConnection(conn);
+})
 
-// app.use('/api/v1' , router)
-// app.use(customErrorHandler)
+app.use('/api/v1' , router)
+app.use(customErrorHandler)
 
 app.get('', (req,res,next)=> {
     res.send('server çalışıyor')
